44476ba81bdb2a75cca6fac956e18c203291a8e2
Copper Phosphate <cu3po42@gmail.com>
2015-08-14 14:08:06 +0200

    Add external link element that opens a link in the default browser
    Since the UI is a webpage, by default a link will be opened in the UI.
    This element cancels that and instead tells the OS to open the link in
    the default browser.

---
+++ app/elements/external-link/external-link.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../../bower_components/polymer-ts/polymer-ts.ts"/>
+/// <reference path="../../../typings/github-electron/github-electron.d.ts" />
+import remote = require("remote");
+
+(() => {
+var openExternal = remote.require("shell").openExternal;
+@component("external-link")
+@extend("a")
+class ExternalLink extends polymer.Base {
+    href: string;
+    @listen("tap")
+    onTap(e) {
+        e.preventDefault();
+        openExternal(this.href);
+    }
+}
+polymer.createElement(ExternalLink);
+})()
