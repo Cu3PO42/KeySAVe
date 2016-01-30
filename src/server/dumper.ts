786dff14b96cd79b6a933f989d519b19295b665e
Copper Phosphate <cu3po42@gmail.com>
2016-01-30 14:34:00 +0100

    Do all breaking calculations in a separate process so the UI isn't locked

--- src/server/dumper.ts
+++ src/server/dumper.ts
@@ -1,96 +1,51 @@
-import * as fs from "fs-extra";
-import * as KeySAV from "keysavcore";
 import { app } from "electron";
-import * as _ from "lodash";
-import * as util from "util";
-import * as path from "path-extra";
 import * as Promise from "bluebird";
 import registerIpc from "electron-ipc-tunnel/server";
-
-function bufToArr(buf: Buffer) {
-    var tmp: Uint8Array = new Uint8Array(buf.length);
-    for (let i = 0; i < buf.length; i++) {
-        tmp[i] = buf.readUInt8(i);
-    }
-    return tmp;
-}
-
-function padNumber(n) {
-    return ("00000" + n).slice(-5);
-}
+import { fork } from "child_process";
 
 export default function() {
-    var dataDirectory = path.join(path.homedir(), "Documents", "KeySAVe", "data");
-    fs.mkdirpSync(dataDirectory);
-    var store = new KeySAV.KeyStoreFileSystem(dataDirectory);
-    KeySAV.setKeyStore(store);
-    app.on("window-all-closed", () => store.close());
+    var worker = fork(__dirname + "/../workers/dumper.js")
 
+    app.on("window-all-closed", () => {
+        worker.send("close");
+    });
+
+    var id: number = 0;
+    var promises: { [id: number]: { resolve: Function, reject: Function} } = {};
     registerIpc("dump-save", async function(reply, args) {
-        var buf = await fs.readFileAsync(args);
-        var arr = bufToArr(buf);
-        var reader = await KeySAV.loadSav(arr);
-        reader.scanSlots();
-        var res = [];
-        var tmp;
-        for (let i = 0; i < 31*30; i++) {
-            tmp = reader.getPkx(i);
-            if (tmp !== undefined) {
-                res.push(tmp);
-            }
-        }
-        return { pokemon: res, isNewKey: reader.isNewKey };
+        worker.send({ cmd: "dump-save", path: args, id: id });
+        return new Promise(function(resolve, reject) {
+            promises[id++] = { resolve: resolve, reject: reject };
+        });
     });
 
     registerIpc("dump-bv", async function(reply, args) {
-        var buf = await fs.readFileAsync(args);
-        var arr = bufToArr(buf);
-        var reader = await KeySAV.loadBv(arr);
-        var myTeam = [], enemyTeam = [];
-        var tmp;
-        for (let i = 0; i < 6; ++i) {
-            tmp = reader.getPkx(i, false);
-            if (tmp !== undefined) {
-                myTeam.push(tmp);
-            }
-            tmp = reader.getPkx(i, true);
-            if (tmp !== undefined) {
-                enemyTeam.push(tmp);
-            }
-        }
-        return {enemyDumpable: reader.dumpsEnemy, myTeam: myTeam, enemyTeam: enemyTeam};
+        worker.send({ cmd: "dump-bv", path: args, id: id });
+        return new Promise(function(resolve, reject) {
+            promises[id++] = { resolve: resolve, reject: reject };
+        });
     });
 
     registerIpc("break-key", async function(reply, args) {
-        var files = await Promise.map([fs.readFileAsync(args.file1), fs.readFileAsync(args.file2)], bufToArr);
-        if (files[0].length === 28256 && files[1].length === 28256) {
-            return await KeySAV.breakBv(files[0], files[1]);
-        } else {
-            files = _.map(files, (f) => f.subarray(f.length % 0x100000));
-            return await KeySAV.breakSav(files[0], files[1]);
-        }
+        worker.send({ cmd: "break-key", file1: args.file1, file2: args.file2, id: id });
+        return new Promise(function(resolve, reject) {
+            promises[id++] = { resolve: resolve, reject: reject };
+        });
     });
 
     registerIpc("break-folder", async function(reply, folder) {
-        await fs.readdirAsync(folder)
-        .map(async (fileName) => {
-            var file = path.join(folder, fileName);
-            var stat = await fs.statAsync(file);
-            if (stat.isDirectory())
-                return;
-            switch (stat.size) {
-                case 0x100000:
-                case 0x10009C:
-                case 0x10019A:
-                    break;
-                default:
-                    return;
-            }
-            var buf = await fs.readFileAsync(file)
-            var arr = bufToArr(buf);
-            var reader = await KeySAV.loadSav(arr);
-            reader.scanSlots();
-        }, { concurrency: 1 });
-        return;
+        worker.send({ cmd: "break-folder", folder: folder, id: id });
+        return new Promise(function(resolve, reject) {
+            promises[id++] = { resolve: resolve, reject: reject };
+        });
+    });
+
+    worker.on("message", function(res) {
+        if (res.e === undefined) {
+            promises[res.id].resolve(res.res);
+        } else {
+            promises[res.id].reject(res.err);
+        }
+        delete promises[res.id];
     });
 };
---
+++ src/workers/dumper.ts
@@ -0,0 +1,127 @@
+import * as KeySAV from "keysavcore";
+import * as path from "path-extra";
+import * as fs from "fs-extra";
+import "../init/promisify-fs";
+
+function bufToArr(buf: Buffer) {
+    var tmp: Uint8Array = new Uint8Array(buf.length);
+    for (let i = 0; i < buf.length; i++) {
+        tmp[i] = buf.readUInt8(i);
+    }
+    return tmp;
+}
+
+var dataDirectory = path.join(path.homedir(), "Documents", "KeySAVe", "data");
+fs.mkdirpSync(dataDirectory);
+var store = new KeySAV.KeyStoreFileSystem(dataDirectory);
+KeySAV.setKeyStore(store);
+
+async function close() {
+    store.close();
+}
+
+async function dumpBv(args) {
+    var buf = await fs.readFileAsync(args.path);
+    var arr = bufToArr(buf);
+    try {
+        var reader = await KeySAV.loadBv(arr);
+        var myTeam = [], enemyTeam = [];
+        var tmp;
+        for (let i = 0; i < 6; ++i) {
+            tmp = reader.getPkx(i, false);
+            if (tmp !== undefined) {
+                myTeam.push(tmp);
+            }
+            tmp = reader.getPkx(i, true);
+            if (tmp !== undefined) {
+                enemyTeam.push(tmp);
+            }
+        }
+        process.send({ res: { enemyDumpable: reader.dumpsEnemy, myTeam: myTeam, enemyTeam: enemyTeam }, id: args.id});
+    } catch (e) {
+        process.send({err: e, id: args.id});
+    }
+}
+
+async function dumpSave(args) {
+    try {
+        var buf = await fs.readFileAsync(args.path);
+        var arr = bufToArr(buf);
+        var reader = await KeySAV.loadSav(arr);
+        reader.scanSlots();
+        var res = [];
+        var tmp;
+        for (let i = 0; i < 31*30; i++) {
+            tmp = reader.getPkx(i);
+            if (tmp !== undefined) {
+                res.push(tmp);
+            }
+        }
+        process.send({ res: { pokemon: res, isNewKey: reader.isNewKey }, id: args.id });
+    } catch (e) {
+        process.send({err: e, id: args.id});
+    }
+}
+
+async function breakKey(args) {
+    try {
+        var files = await Promise.map([fs.readFileAsync(args.file1), fs.readFileAsync(args.file2)], bufToArr);
+        if (files[0].length === 28256 && files[1].length === 28256) {
+            var res = await KeySAV.breakBv(files[0], files[1]);
+            process.send({ res: res, id: args.id });
+        } else {
+            files = _.map(files, (f) => f.subarray(f.length % 0x100000));
+            var res = await KeySAV.breakSav(files[0], files[1]);
+            process.send({ res: res, id: args.id });
+        }
+    } catch (e) {
+        process.send({err: e, id: args.id});
+    }
+}
+
+async function breakFolder(args) {
+    try {
+        await fs.readdirAsync(args.folder)
+        .map(async (fileName) => {
+            var file = path.join(args.folder, fileName);
+            var stat = await fs.statAsync(file);
+            if (stat.isDirectory())
+                return;
+            switch (stat.size) {
+                case 0x100000:
+                case 0x10009C:
+                case 0x10019A:
+                    break;
+                default:
+                    return;
+            }
+            var buf = await fs.readFileAsync(file)
+            var arr = bufToArr(buf);
+            var reader = await KeySAV.loadSav(arr);
+            reader.scanSlots();
+        });
+        process.send({ id: args.id });
+    } catch (e) {
+        process.send({ err: e, id: args.id });
+    }
+}
+
+process.on("message", function(m) {
+    switch(m.cmd) {
+        case "dumb-bv":
+            dumpBv(m);
+            break;
+        case "dump-save":
+            dumpSave(m);
+            break;
+        case "break-key":
+            breakKey(m);
+            break;
+        case "break-folder":
+            breakFolder(m);
+            break;
+        case "close":
+            close();
+            break;
+    }
+});
