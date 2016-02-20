c7e60777ae8670047a289815c4518a4cbe04638b
Copper Phosphate <cu3po42@gmail.com>
2016-02-20 15:45:11 +0100

    Extract presentation of Dumping into component

---
+++ app/components/Dumping.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import DumpingFileOpener from '../components/DumpingFileOpener';
+import PkmList from '../components/PkmList';
+import Dialog from 'material-ui/lib/dialog';
+import FlatButton from 'material-ui/lib/flat-button';
+
+const Dumping = ({ name, openFile, backup, type, goodKey, filter, setFilterBv, setFilterSav, pokemon, error, closeDialog }) => (
+  <div>
+      <DumpingFileOpener
+        file={name}
+        fileOpened={openFile}
+        backup={backup}
+        type={type}
+        goodKey={goodKey}
+        lowerBox={filter.lower}
+        upperBox={filter.upper}
+        isOpponent={filter.isOpponent}
+        bvFilterChanged={setFilterBv}
+        savFilterChanged={setFilterSav}
+      />
+      <PkmList pokemon={pokemon} />
+      <Dialog
+        modal
+        open={error !== undefined}
+        actions={[<FlatButton label="Ok" primary onTouchTap={closeDialog}/>]}
+      >
+        {((e) => {
+          switch (e.name) {
+            case 'NoKeyAvailableError':
+              return `You have to break for this ${e.keyType === 'SAV' ? 'save' : 'battle video'} first!`;
+            case 'NotASaveOrBattleVideoError':
+              return `This file is neither a supported save nor a supported battle video.`;
+            default:
+              return `An unknown error occured: ${e}`;
+          }
+        })(error || {})}
+      </Dialog>
+  </div>
+);
+
+export default Dumping;
--- app/containers/DumpingContainer.js
+++ app/containers/DumpingContainer.js
@@ -1,18 +1,14 @@
-import React from 'react';
 import { createSelector } from 'reselect';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Seq } from 'immutable';
-import DumpingFileOpener from '../components/DumpingFileOpener';
 import { openFile, dismissError } from '../actions/file';
 import { setFilterBv, setFilterSav } from '../actions/filter';
 import { openDialog } from '../actions/dialog';
-import PkmList from '../components/PkmList';
-import Dialog from 'material-ui/lib/dialog';
-import FlatButton from 'material-ui/lib/flat-button';
 import { send } from 'electron-ipc-tunnel/client';
 import * as fse from 'fs-extra';
 import * as path from 'path';
+import Dumping from '../components/Dumping';
 
 const getPokemonSeq = createSelector(
   state => state.file.pokemon,
@@ -59,8 +55,8 @@ function mapDispatchToProps() {
         try {
           const name = fileType === 'SAV' ? 'Save' : 'Battle Video';
           var dest = await send('file-dialog-save',
-                  { options: { filters: [{ name,
-                                          extensions: [path.extname(file).slice(1)] }] } });
+                                { options: { filters: [{ name,
+                                             extensions: [path.extname(file).slice(1)] }] } });
           await fse.copyAsync(file, dest);
           dispatch(openDialog(`${name} backupped!`));
         } catch (e) {
@@ -72,39 +68,5 @@ function mapDispatchToProps() {
     return res;
   };
 }
-
-const Dumping = ({ name, openFile, backup, type, goodKey, filter, setFilterBv, setFilterSav, pokemon, error, closeDialog }) => (
-  <div>
-      <DumpingFileOpener
-        file={name}
-        fileOpened={openFile}
-        backup={backup}
-        type={type}
-        goodKey={goodKey}
-        lowerBox={filter.lower}
-        upperBox={filter.upper}
-        isOpponent={filter.isOpponent}
-        bvFilterChanged={setFilterBv}
-        savFilterChanged={setFilterSav}
-      />
-      <PkmList pokemon={pokemon} />
-      <Dialog
-        modal
-        open={error !== undefined}
-        actions={[<FlatButton label="Ok" primary onTouchTap={closeDialog}/>]}
-      >
-        {((e) => {
-          switch (e.name) {
-            case 'NoKeyAvailableError':
-              return `You have to break for this ${e.keyType === 'SAV' ? 'save' : 'battle video'} first!`;
-            case 'NotASaveOrBattleVideoError':
-              return `This file is neither a supported save nor a supported battle video.`;
-            default:
-              return `An unknown error occured: ${e}`;
-          }
-        })(error || {})}
-      </Dialog>
-  </div>
-);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dumping);
