56ba2fbacfee44711f3d1a3dd9786b09ad397557
Copper Phosphate <cu3po42@gmail.com>
2016-04-10 19:07:33 +0200

    Use react-swipeable-views

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 import { Tabs, Tab } from 'material-ui';
+import SwipeableViews from 'react-swipeable-views';
 import DumpingContainer from '../containers/DumpingContainer';
 import FormattingContainer from '../containers/FormattingContainer';
 import DialogContainer from '../containers/DialogContainer';
 import BreakingContainer from '../containers/BreakingContainer';
 import styles from './Home.module.scss';
+
+const tabs = ['dumping', 'options', 'breaking'];
 
 export default class Home extends Component {
   static propTypes = {
@@ -20,25 +23,31 @@ export default class Home extends Component {
     this.context.router.push(`/main/${tab}`);
   }
 
+  tabIndexChanged = (tab) => {
+    this.tabChanged(tabs[tab]);
+  }
+
   render() {
     return (
-      <div>
+      <div className={styles.mainWrapper}>
         <DialogContainer />
         <Tabs
-          className={styles.tabs}
           value={this.props.params.tab}
           onChange={this.tabChanged}
         >
-          <Tab label="Dumping" value="dumping">
-            <DumpingContainer />
-          </Tab>
-          <Tab label="Options" value="options">
-            <FormattingContainer />
-          </Tab>
-          <Tab label="Breaking" value="breaking">
-            <BreakingContainer />
-          </Tab>
+          <Tab label="Dumping" value="dumping" />
+          <Tab label="Options" value="options" />
+          <Tab label="Breaking" value="breaking" />
         </Tabs>
+        <SwipeableViews
+          index={tabs.indexOf(this.props.params.tab)}
+          onChangeIndex={this.tabIndexChanged}
+          className={styles.stretch}
+        >
+          <DumpingContainer />
+          <FormattingContainer />
+          <BreakingContainer />
+        </SwipeableViews>
       </div>
     );
   }
