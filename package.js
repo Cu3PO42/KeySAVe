bb85300fa928ce584cfbf32b8d354545817e3877
Copper Phosphate <cu3po42@gmail.com>
2016-06-02 16:56:13 +0200

    Upgrate to electron 1.2.1 for DevTools support

--- package.js
+++ package.js
@@ -67,7 +67,7 @@ if (DEFAULT_OPTS.version) {
   // use the same version as the currently-installed electron-prebuilt
   exec('npm list electron-prebuilt', (err, stdout) => {
     if (err) {
-      DEFAULT_OPTS.version = '1.1.2';
+      DEFAULT_OPTS.version = '1.2.1';
     } else {
       DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
     }
