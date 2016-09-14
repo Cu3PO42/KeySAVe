4f036195690211bf68edcff4069887466b70124f
Copper Phosphate <cu3po42@gmail.com>
2016-09-14 19:13:06 +0200

    Store log in configuration folder instead of appData folder. Closes #125

--- serversrc/logger.js
+++ serversrc/logger.js
@@ -2,7 +2,7 @@ import { Logger, transports } from 'winston';
 import { app } from 'electron';
 import registerIpcOrg from 'electron-ipc-tunnel/server';
 
-const file = process.env.NODE_ENV === 'development' ? './keysave.log' : `${app.getPath('appData')}/keysave.log`;
+const file = process.env.NODE_ENV === 'development' ? './keysave.log' : `${app.getPath('userData')}/keysave.log`;
 
 const logger = new Logger({
   transports: [
