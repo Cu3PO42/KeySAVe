0d382c1300bae20d335359549894469f402b56bd
Copper Phosphate <cu3po42@gmail.com>
2016-11-18 14:17:22 +0100

    Replace electron-prebuilt with electron

--- package.js
+++ package.js
@@ -67,11 +67,11 @@ if (DEFAULT_OPTS.version) {
   buildServer(startPack);
 } else {
   // use the same version as the currently-installed electron-prebuilt
-  exec('npm list electron-prebuilt', (err, stdout) => {
+  exec('npm list electron', (err, stdout) => {
     if (err) {
       DEFAULT_OPTS.version = '1.4.7';
     } else {
-      DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
+      DEFAULT_OPTS.version = stdout.split('electron@')[1].replace(/\s/g, '');
     }
 
     buildServer(startPack);
