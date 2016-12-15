74ab675fb7a762ca61738a676603c07aa4674d85
Copper Phosphate <cu3po42@gmail.com>
2016-12-15 20:53:06 +0100

    Update Handlebars templates for new TID handling
    - The CSV formats will always show the TID6 so all data is exported
    - The old TSV template now exists as TSV (Legacy)
    - The new TSV template does not include SID and will show the TID as
      shown by the game

--- app/components/formatters/handlebars/index.js
+++ app/components/formatters/handlebars/index.js
@@ -30,10 +30,18 @@ export const defaultOptions = [{
     splitBoxes: false
   }
 }, {
-  name: 'TSV',
+  name: 'TSV (Legacy)',
   format: {
     header: '| Box | Slot | OT | TID | SID | TSV |<br />|:---:|:---:|:---:|:---:|:---:|:---:|',
-    format: '| {{#if isGhost}}~{{/if}}B{{box}} | {{row}},{{column}} | {{ot}} | {{tid}} | {{sid}} | {{tsv}} |',
+    format: '| {{#if isGhost}}~{{/if}}B{{box}} | {{row}},{{column}} | {{ot}} | {{tid6}} | {{sid}} | {{tsv}} |',
+    boxHeader: '<h3 style="margin-bottom: 0; font-family: Roboto, sans-serif; color: #999">Box {{box}}</h3>',
+    splitBoxes: false
+  }
+}, {
+  name: 'TSV',
+  format: {
+    header: '| Box | Slot | OT | TID | TSV |<br />|:---:|:---:|:---:|:---:|:---:|:---:|',
+    format: '| {{#if isGhost}}~{{/if}}B{{box}} | {{row}},{{column}} | {{ot}} | {{tid}} | {{tsv}} |',
     boxHeader: '<h3 style="margin-bottom: 0; font-family: Roboto, sans-serif; color: #999">Box {{box}}</h3>',
     splitBoxes: false
   }
@@ -41,7 +49,7 @@ export const defaultOptions = [{
   name: 'CSV',
   format: {
     header: 'Box,Row,Column,Species,Gender,Nature,Ability,HP IV,ATK IV,DEF IV,SPA IV,SPD IV,SPE IV,HP Type,ESV,TSV,Nickname,OT,Ball,TID,SID,HP EV,ATK EV,DEF EV,SPA EV,SPD EV,SPE EV,Move 1,Move 2,Move 3,Move 4,Relearn 1,Relearn 2,Relearn 3,Relearn 4,Shiny,Egg',
-    format: '{{#if isGhost}}~{{/if}}{{box}},{{row}},{{column}},{{speciesName}},{{genderString gender}},{{natureName}},{{abilityName}},{{ivHp}},{{ivAtk}},{{ivDef}},{{ivSpAtk}},{{ivSpDef}},{{ivSpe}},{{typeName hpType}},{{esv}},{{tsv}},{{nickname}},{{ot}},{{ballName}},{{tid}},{{sid}},{{evHp}},{{evAtk}},{{evDef}},{{evSpAtk}},{{evSpDef}},{{evSpe}},{{moveName move1}},{{moveName move2}},{{moveName move3}},{{moveName move4}},{{moveName eggMove1}},{{moveName eggMove2}},{{moveName eggMove3}},{{moveName eggMove4}},{{isShiny}},{{isEgg}}',
+    format: '{{#if isGhost}}~{{/if}}{{box}},{{row}},{{column}},{{speciesName}},{{genderString gender}},{{natureName}},{{abilityName}},{{ivHp}},{{ivAtk}},{{ivDef}},{{ivSpAtk}},{{ivSpDef}},{{ivSpe}},{{typeName hpType}},{{esv}},{{tsv}},{{nickname}},{{ot}},{{ballName}},{{tid6}},{{sid}},{{evHp}},{{evAtk}},{{evDef}},{{evSpAtk}},{{evSpDef}},{{evSpe}},{{moveName move1}},{{moveName move2}},{{moveName move3}},{{moveName move4}},{{moveName eggMove1}},{{moveName eggMove2}},{{moveName eggMove3}},{{moveName eggMove4}},{{isShiny}},{{isEgg}}',
     boxHeader: '<div style="margin-bottom: 20px;"></div>',
     splitBoxes: false
   }
@@ -49,7 +57,7 @@ export const defaultOptions = [{
   name: 'CSV (Raw Data)',
   format: {
     header: 'Box,Row,Column,Species,Gender,Nature,Ability,HP IV,ATK IV,DEF IV,SPA IV,SPD IV,SPE IV,HP Type,ESV,TSV,Nickname,OT,Ball,TID,SID,HP EV,ATK EV,DEF EV,SPA EV,SPD EV,SPE EV,Move 1,Move 2,Move 3,Move 4,Relearn 1,Relearn 2,Relearn 3,Relearn 4,Shiny,Egg',
-    format: '{{#if isGhost}}~{{/if}}{{box}},{{row}},{{column}},{{species}},{{gender}},{{nature}},{{ability}},{{ivHp}},{{ivAtk}},{{ivSpAtk}},{{ivSpDef}},{{ivSpe}},{{hpType}},{{esv}},{{tsv}},{{nickname}},{{ot}},{{ball}},{{tid}},{{sid}},{{evHp}},{{evAtk}},{{evDef}},{{evSpAtk}},{{evSpDef}},{{evSpe}},{{move1}},{{move2}},{{move3}},{{move4}},{{eggMove1}},{{eggMove2}},{{eggMove3}},{{eggMove4}},{{isShiny}},{{isEgg}}',
+    format: '{{#if isGhost}}~{{/if}}{{box}},{{row}},{{column}},{{species}},{{gender}},{{nature}},{{ability}},{{ivHp}},{{ivAtk}},{{ivSpAtk}},{{ivSpDef}},{{ivSpe}},{{hpType}},{{esv}},{{tsv}},{{nickname}},{{ot}},{{ball}},{{tid6}},{{sid}},{{evHp}},{{evAtk}},{{evDef}},{{evSpAtk}},{{evSpDef}},{{evSpe}},{{move1}},{{move2}},{{move3}},{{move4}},{{eggMove1}},{{eggMove2}},{{eggMove3}},{{eggMove4}},{{isShiny}},{{isEgg}}',
     boxHeader: '<div style="margin-bottom: 20px;"></div>',
     splitBoxes: false
   }
