5951208de16cbaea70cef11bcb3b3f834eb07c43
Cu3PO42 <cu3po42@gmail.com>
2017-12-23 23:25:15 +0100

    Adjust external link

--- app/components/ExternalLink.js
+++ app/components/ExternalLink.js
@@ -1,17 +1,4 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { remote } from 'electron';
-const { openExternal } = remote.require('electron').shell;
-
-function openLink(e) {
-  e.preventDefault();
-  openExternal(e.target.href);
-}
-
 const ExternalLink = (props) => (
-  <a {...props} onClick={openLink}>{props.children}</a>
+  <a {...props} target="_blank">{props.children}</a>
 );
-ExternalLink.propTypes = {
-  children: PropTypes.node.isRequired
-};
 export default ExternalLink;
