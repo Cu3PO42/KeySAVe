c602cb258d0d6156a581a9ae9767e8283cf0a8f9
Cu3PO42 <cu3po42@gmail.com>
2017-12-23 23:26:07 +0100

    Remove event listeners

--- app/index.js
+++ app/index.js
@@ -5,18 +5,15 @@ import { Router, hashHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import '../init';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import tapEventPlugin from 'react-tap-event-plugin';
 import configure from './configuration';
-import listenToEvents from './event-listener';
 import './app.scss';
 
 tapEventPlugin();
 const store = configureStore();
 configure(store);
-listenToEvents(store);
 
 const muiTheme = getMuiTheme(lightBaseTheme);
 
