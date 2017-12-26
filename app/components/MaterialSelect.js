d7e5119da56eb23c57397dbe7b1ed00dfe33bc64
Cu3PO42 <cu3po42@gmail.com>
2017-12-26 13:23:02 +0100

    Update MaterialSelect

--- app/components/MaterialSelect.js
+++ app/components/MaterialSelect.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 import './MaterialSelect/default.scss';
+import './MaterialSelect.scss'
 
 const MaterialSelect = (props) => (
   <Select
